c6f05cb2f26947e6536eace1a9c92f38d2d5ab47
Jiayu <wangjiayu1917@hotmail.com>
2017-02-14 22:43:12 -0600

    Add files via upload

--- clustering/kMeans.py
+++ clustering/kMeans.py
@@ -2,9 +2,10 @@
 # Kmeans Algorithm
 # Jiayu Wang
 from numpy import *
+import matplotlib.pyplot as plt
 
 class build(object):
-	def __init__(self, k=2, dist='euclidean'):
+	def __init__(self, k=2, dist='euclidean', method='KMeans'):
 		self.k = k					# number of clusters
 		self.dist = dist			# distance function
 		self.method = 'KMeans'		# method: KMeans/biKMeans
@@ -20,7 +21,7 @@ class build(object):
 		for j in range(n):
 			minJ = min(dataSet[:,j])
 			rangeJ = float(max(dataSet[:,j]) - minJ)
-			centroids[:,j] = minJ + rangeJ * random.rand(k,1)
+			centroids[:,j] = minJ + rangeJ * random.rand(self.k,1)
 		return centroids
 	
 	# The k-means clustering algorithm
@@ -28,7 +29,7 @@ class build(object):
 		m = shape(dataSet)[0]
 		# Second col is to store the errors
 		clusterAssment = mat(zeros((m,2)))
-		centroids = createCent(dataSet, self.k)
+		centroids = createCent(dataSet)
 		clusterChanged = True
 		# Until no cluster change
 		while clusterChanged:
@@ -36,7 +37,7 @@ class build(object):
 			for i in range(m):
 				minDist = inf;	minIndex = -1
 				# Find the closest centroid
-				for j in range(k):
+				for j in range(self.k):
 					distJI = distMeas(centroids[j,:], dataSet[i,:])
 					if distJI < minDist:
 						minDist = distJI; minIndex = j
@@ -44,7 +45,7 @@ class build(object):
 				clusterAssment[i,:] = minIndex, minDist**2
 			print(centroids)
 			# Update centroid location
-			for cent in range(k):
+			for cent in range(self.k):
 				ptsInClust = dataSet[nonzero(clusterAssment[:,0].A==cent)[0]]
 				centroids[cent,:] = mean(ptsInClust, axis = 0)
 		return centroids, clusterAssment
@@ -61,7 +62,7 @@ class build(object):
 		for j in range(m):
 			clusterAssment[j,1] = distMeas(mat(centroid0), dataSet[j,:])**2
 		# create k clusters
-		while (len(centList)<k):
+		while (len(centList)<self.k):
 			lowestSSE = inf
 			# Try splitting every cluster
 			for i in range(len(centList)):
@@ -90,14 +91,17 @@ class build(object):
 	# clustering
 	def cluster(self, dataSet):
 		if self.method == 'KMeans':
-			return self.kMeans(dataSet, distMeas=self.distEclud, createCent=self.randCent)
+			return self.KMeans(dataSet, distMeas=self.distEclud, createCent=self.randCent)
 		elif self.method == 'biKMeans':
 			return self.biKMeans(dataSet, distMeas=self.distEclud)
 		else:
 			print('Method not recognized')
 	
-	#Plot two features with class label
-	def view(self,featName):
-		pass
+#Plot clusters
+def view(dataSet,centers,clusters):
+	fig = plt.figure()
+	ax = plt.subplot(111)
+	ax.plot([i for i in range(len(dataSet.label))],centers.tolist()[0])
+	plt.show()
 
 
